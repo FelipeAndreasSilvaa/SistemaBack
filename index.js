ac970650f5e60ea5e6248cb47a8aebb5d83b6636
Felipe Andreas Silva <134702945+FelipeAndreasSilvaa@users.noreply.github.com>
2025-05-24 12:18:47 -0300

    Mudanca no cors

--- index.js
+++ index.js
@@ -48,7 +48,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
   origin: function (origin, callback) {
-    const allowed = ["https://ecommerce-shop-taupe.vercel.app/"];
+    const allowed = [    
+      "https://ecommerce-shop-taupe.vercel.app",
+      "https://ecommerce-shop.vercel.app"
+    ];
     if (!origin || allowed.includes(origin)) {
       callback(null, true);
     } else {
